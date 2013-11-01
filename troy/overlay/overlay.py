79ec5ac0990ec7472ef9840a93a2454a3a92c946
Andre Merzky <andre@merzky.net>
2013-11-02 00:16:58 +0100

    snap

--- troy/overlay/overlay.py
+++ troy/overlay/overlay.py
@@ -16,7 +16,55 @@ Represent a pilot-based overlay that is managed by TROY.
 @ru.Lockable
 class Overlay (object) :
     """
-    """
+    The `Overlay` class represents a resource overlay which is managed by Troy,
+    i.e. in application and user space.  It contains a set of :class:`Pilots`, 
+    which represent the resource capabilities of the overlay.
+
+    Overlay instances are owned by the :class:`OverlayManager` class -- only
+    that class should change its composition and state (but Overlay may get
+    created by the :class:`troy.Planner`).
+
+    An overlay undergoes a series of transformations before being able to run
+    workloads.  Those transformations are orchestrated by the overlay manager.
+    To support that orchestration, an overlay will be lockable, and it will have
+    a state attribute.  The valid states are listed below.
+
+    Internally, a overlay is represented by a set of :class:`troy.Pilot`
+    instances.  As the overlay undergoes transformations, it is enriched by
+    additional information, although those are kept solely within the
+    :class:`Pilot` instances -- see there for more details.
+
+    The overlay transformations are:
+
+    * *Scheduling:* A overlay is mapped onto resourced.
+
+    * *Dispatching:* A scheduled overlay is dispatched to the respective
+      resources.
+
+
+    Overlay States
+    ---------------
+
+    An overlay can be in different states, depending on the transformations
+    performed on it.  Specifically, it can be in `DESCRIBED`, `SCHEDULED`,
+    `DISPATCHED`, `COMPLETED` or `FAILED`.
+
+    A overlay enters the overlay manager in `DESCRIBED` state, and all follow-up
+    state transitions are kept within the overlay manager.
+
+    Those states are ill defined in case of partial transformations -- if, for
+    example, a translation step only derives compute units for some of the
+    tasks, but not for others.  As a general rule, a overlay will remain in
+    a state until the transformation has been performed on all applicable
+    overlay components (tasks and relations).
+
+    Even on fully transformed overlay, the actual overlay state may not be
+    trivial to determine -- for example, a specific compute unit configuration
+    derived in a translation step may show to be impossible to dispatch later
+    on, and may require a re-translation into a different configuration; or if
+    a newly `DESCRIBED` task is added to a `SCHEDULED` overlay.   Those
+    feedback loops are considered out-of-scope for Troy at this point, so that
+    state transitions are considered irreversible.  
 
     pass
 
