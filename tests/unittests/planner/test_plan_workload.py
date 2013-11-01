382c8b371204a95b63e85a41e486aa9d431872d1
Mark Santcroos <mas781@scarletmail.rutgers.edu>
2013-11-01 22:38:05 +0100

    Add test for planner.
    Change workload_input accordingly.

---
+++ tests/unittests/planner/test_plan_workload.py
@@ -0,0 +1,41 @@
+
+__author__    = "Mark Santcroos"
+__copyright__ = "Copyright 2013, RADICAL @ Rutgers"
+__license__   = "MIT"
+
+
+import pprint
+import radical.utils.testing as rut
+
+import troy
+
+
+# ------------------------------------------------------------------------------
+#
+def test_plan_workload():
+    """
+    test workload planning
+    """
+    tc = rut.get_test_config()
+    wl_dict = tc.workload_dict
+
+    wl = troy.Workload()
+    planner = troy.Planner()
+    pprint.pprint(wl_dict)
+
+    if not 'tasks' in wl_dict:
+        assert False, "no tasks in workload dict"
+
+    if not len(wl_dict['tasks']):
+        assert False, "zero tasks in workload dict"
+
+    for task_dict in wl_dict['tasks']:
+        task_description = troy.TaskDescription(task_dict)
+        wl.add_task(task_description)
+
+    for relation_dict in wl_dict['relations']:
+        relation_description = troy.RelationDescription(relation_dict)
+        wl.add_relation(relation_description)
+
+    planner.plan(wl)
+
