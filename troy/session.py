12de77efae31d27cd6be01c370532badf6f86f80
Andre Merzky <andre@merzky.net>
2013-12-02 17:36:21 +0100

    add missing file...

---
+++ troy/session.py
@@ -0,0 +1,12 @@
+
+__author__    = "TROY Development Team"
+__copyright__ = "Copyright 2013, RADICAL"
+__license__   = "MIT"
+
+
+import saga
+
+
+class Session (saga.Session) : pass
+class Context (saga.Context) : pass
+
