330b4644bfad9b71918429af643539eef8c6fd85
Andre Merzky <andre@merzky.net>
2013-12-04 20:49:34 +0100

    remove  guard for now

--- examples/wip_potentially_broken_debug_schedule.py
+++ examples/wip_potentially_broken_debug_schedule.py
@@ -55,7 +55,7 @@ if __name__ == '__main__':
         planner.expand_workload(workload.id)
 
         # Initial description of the overlay based on the workload
-        overlay_id = planner.derive_overlay(workload.id, guard=troy.UPPER_LIMIT)
+        overlay_id = planner.derive_overlay(workload.id)
         #overlay_id = planner.derive_overlay(workload.id)
 
         # Translate 1 workload into N ComputeUnits and N DataUnits
