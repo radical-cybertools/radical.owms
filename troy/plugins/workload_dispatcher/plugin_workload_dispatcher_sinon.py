17a0c3e21efb8f23906d5bfbe6d65128249d9b12
Andre Merzky <andre@merzky.net>
2014-01-15 17:28:33 +0100

    coord_url support for sinon plugins

--- troy/plugins/workload_dispatcher/plugin_workload_dispatcher_sinon.py
+++ troy/plugins/workload_dispatcher/plugin_workload_dispatcher_sinon.py
@@ -6,8 +6,6 @@ import radical.utils as ru
 from   troy.constants import *
 import troy
 
-DBURL  = 'mongodb://ec2-184-72-89-141.compute-1.amazonaws.com:27017/'
-FGCONF = 'https://raw.github.com/saga-project/saga-pilot/master/configs/futuregrid.json'
 
 # ------------------------------------------------------------------------------
 #
@@ -46,9 +44,21 @@ class PLUGIN_CLASS (troy.PluginBase):
         overlay managers!
         """
 
-        troy._logger.info ("init the sinon workload dispatcher plugin")
-        
-        self._sinon  = sinon.Session (database_url = DBURL)
+        self._coord = None
+
+        if  'COORDINATION_URL' in os.environ :
+            self._coord = os.environ['COORDINATION_URL'] 
+
+        elif 'coordination_url' in self.cfg :
+            self._coord = self.cfg['coordination_url']
+
+        else :
+            troy._logger.error ("No COORDINATION_URL set for sinon backend")
+            troy._logger.info  ("example: export COORDINATION_URL=redis://<pass>@gw68.quarry.iu.teragrid.org:6379")
+            troy._logger.info  ("Contact Radica@Ritgers for the redis password")
+            raise RuntimeError ("Cannot use sinon backend - no COORDINATION_URL -- see debug log for details")
+
+        self._sinon  = sinon.Session (database_url = self._coord)
 
 
     # --------------------------------------------------------------------------
