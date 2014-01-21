0cd578c0d5981f6492e7c59a0fd33fea4fc706de
Mark Santcroos <mas781@scarletmail.rutgers.edu>
2014-01-20 23:22:16 -0500

    Trying to get it to work...

--- examples/gromacs_demo.py
+++ examples/gromacs_demo.py
@@ -1,6 +1,6 @@
 
 __author__    = "TROY Development Team"
-__copyright__ = "Copyright 2013, RADICAL"
+__copyright__ = "Copyright 2014, RADICAL"
 __license__   = "MIT"
 
 
@@ -27,19 +27,47 @@ if __name__ == '__main__':
 
     os.chdir("/tmp/troy_demo/")
 
+    # Create a session for TROY.
+    session = troy.Session(
+        {
+            'planner_concurrent': {
+                'concurrency': 100
+            },
+            'workload_dispatcher_bigjob_pilot': {
+                'coordination_url ': 'redis://ILikeBigJob_wITH-REdIS@gw68.quarry.iu.teragrid.org:6379'
+            },
+            'overlay_provisioner_bigjob_pilot': {
+                'coordination_url ': 'redis://ILikeBigJob_wITH-REdIS@gw68.quarry.iu.teragrid.org:6379'
+            },
+        })
+
+    # Add resources to the session.
+    session.user_cfg['overlay_scheduler_round_robin'] = {
+        'resources': 'pbs+ssh://india.futuregrid.org/'
+    }
+
+    # Manage credentials.
+    # TODO: set it to args.protocol (default ssh).
+    c1 = troy.Context ('ssh')
+    c1.user_id = 'marksant'
+    session.add_context (c1)
+
     # create a data stager for all workloads
     stager = troy.DataStager()
 
     # Responsible for application workload
     workload_mgr = troy.WorkloadManager(dispatcher = 'bigjob_pilot',
-                                        stager     = stager)  # this is actually the default
+                                        stager     = stager,  # this is actually the default
+                                        session    = session)
 
     # Responsible for managing the pilot overlay
     overlay_mgr = troy.OverlayManager(scheduler    = 'round_robin',
-                                      provisioner  = troy.AUTOMATIC)
+                                      provisioner  = 'bigjob_pilot',
+                                      session      = session)
 
     # Planning makes initial mapping of workload to overlay
-    planner = troy.Planner (planner='concurrent')
+    planner = troy.Planner (planner='concurrent',
+                            session=session)
 
     # Create a task for every radicalist
     task_descriptions = list()
@@ -59,6 +87,7 @@ if __name__ == '__main__':
         print task_descr
 
         task_descriptions.append(task_descr)
+
 
     workload_id = workload_mgr.create_workload(task_descriptions)
 
