66d771774746f2d38d51733511bff6fb7e97c814
Matteo Turilli <matteo.turilli@gmail.com>
2014-01-23 22:56:13 -0500

    Tags timer logs.

--- troy/utils/time_it.py
+++ troy/utils/time_it.py
@@ -1,20 +1,19 @@
 
-
 import time
 import troy
-
 
 def timeit (method) :
 
     def timed (*args, **kwargs) :
+
         start  = time.time ()
         result = method (*args, **kwargs)
         stop   = time.time ()
 
-        troy._logger.info ('%30s : %6.2f sec' \
-                        % ("%s (%s, %s) " % (method.__name__, args, kwargs), 
-                           stop-start))
+	signature = "TIMER %s (%s, %s) " % (method.__name__, args, kwargs)
+
+        troy._logger.info ('%30s : %6.2f sec' % (signature, stop-start))
+
         return result
 
     return timed
-
