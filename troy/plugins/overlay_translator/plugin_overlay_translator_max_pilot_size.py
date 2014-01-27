e6eb5a3a57bc7170696ce770dbb8350d6f4ff8e6
Andre Merzky <andre@merzky.net>
2014-01-27 11:16:52 +0100

    add log info about max pilot size

--- troy/plugins/overlay_translator/plugin_overlay_translator_max_pilot_size.py
+++ troy/plugins/overlay_translator/plugin_overlay_translator_max_pilot_size.py
@@ -48,6 +48,8 @@ class PLUGIN_CLASS (troy.PluginBase):
         else :
             pilot_size = _DEFAULT_PILOT_SIZE
 
+        troy._logger.info ("overlay  translate: max pilot size set to %d" % pilot_size(
+
         pilot_cnt = 0
         while (pilot_cnt * pilot_size) < overlay.description.cores :
 
