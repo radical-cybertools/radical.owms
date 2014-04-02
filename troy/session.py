e5ec65ee1e508bba383b51d7dc50eb2d0b8d8dc3
Andre Merzky <andre@merzky.net>
2014-04-02 13:40:14 +0200

    user smaller default walltimes

--- troy/session.py
+++ troy/session.py
@@ -75,7 +75,7 @@ _resource_config_skeleton = {
         'username'  : None,
         'home'      : None,
         'queue'     : None,
-        'walltime'  : int(60*24) # default: 1 day
+        'walltime'  : int(60) # default: 1 hour
     }
 
 
